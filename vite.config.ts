ff495b24b3a143672be67a4761b33f8810939730
Sandeep Kumbar <sandeep@Sandeeps-MacBook-Pro.local>
2026-01-15 16:56:10 +0530

    chore: configure github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/personal_portfolio/",
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
 });
